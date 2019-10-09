051a04363d44b1a9f3f7bdabfb99961553a6de08
Alex Nichol <unixpickle@gmail.com>
2019-10-09 07:24:37 -0400

    more reasonable average size

--- reptile_gen/reptile.py
+++ reptile_gen/reptile.py
@@ -18,12 +18,14 @@ def interpolate_parameters(parameters, epsilon, fn):
     return res
 
 
-def run_sgd_epoch(model, inputs, outputs, optimizer):
+def run_sgd_epoch(model, inputs, outputs, optimizer, batch=1):
     device = next(model.parameters()).device
     losses = []
-    for x, y in zip(inputs, outputs):
-        out = model(torch.from_numpy(x[None]).to(device).long())
-        target = torch.from_numpy(y[None]).to(device).float()
+    for i in range(0, len(inputs), batch):
+        x = inputs[i:i+batch]
+        y = outputs[i:i+batch]
+        out = model(torch.from_numpy(x).to(device).long())
+        target = torch.from_numpy(y).to(device).float()
         loss = F.binary_cross_entropy_with_logits(out, target)
         optimizer.zero_grad()
         loss.backward()
--- train.py
+++ train.py
@@ -10,22 +10,23 @@ from reptile_gen.reptile import reptile_step
 
 OUT_PATH = 'model.pt'
 OPTIM_PATH = 'optimizer.pt'
+AVG_SIZE = 1000
 
 
 def main():
     model = MNISTModel()
     if os.path.exists(OUT_PATH):
         model.load_state_dict(torch.load(OUT_PATH))
-    last_100 = []
+    last_n = []
     opt = optim.Adam(model.parameters(), betas=(0, 0.999))
     if os.path.exists(OPTIM_PATH):
         opt.load_state_dict(torch.load(OPTIM_PATH))
     for i, (inputs, outputs) in enumerate(iterate_mini_datasets()):
         losses = reptile_step(model, inputs, outputs, opt)
         loss = np.mean(losses)
-        last_100.append(loss)
-        last_100 = last_100[-100:]
-        print('step %d: loss=%f last_100=%f' % (i, np.mean(losses), np.mean(last_100)))
+        last_n.append(loss)
+        last_n = last_n[-AVG_SIZE:]
+        print('step %d: loss=%f last_%d=%f' % (i, np.mean(losses), AVG_SIZE, np.mean(last_n)))
         if i % 100 == 0:
             torch.save(model.state_dict(), OUT_PATH)
             torch.save(opt.state_dict(), OPTIM_PATH)
