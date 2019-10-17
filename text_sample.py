cc9fba55960f9d350bb67269d96396def94ff037
Alex Nichol <unixpickle@gmail.com>
2019-10-17 15:32:09 -0400

    use best device in text_sample.py

--- text_sample.py
+++ text_sample.py
@@ -3,27 +3,30 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
+from reptile_gen.device import best_available_device
 from reptile_gen.model import TextModel
 
 from text_train import OUT_PATH
 
 
 def main():
+    device = torch.device(best_available_device())
     model = TextModel()
     model.load_state_dict(torch.load(OUT_PATH))
+    model.to(device)
     opt = optim.SGD(model.parameters(), lr=1e-3)
 
     sequence = []
 
     for i in range(128):
-        inputs = torch.from_numpy(np.array([[i]])).long()
+        inputs = torch.from_numpy(np.array([[i]])).to(device).gradlong()
         logits = model(inputs)
         probs = F.softmax(logits[0], dim=0).detach().cpu().numpy()
         sample = np.random.choice(np.arange(256), p=probs)
         sequence.append(int(sample))
         if sample == 0:
             break
-        targets = torch.from_numpy(np.array([sample])).long()
+        targets = torch.from_numpy(np.array([sample])).to(device).long()
         loss = F.cross_entropy(logits, targets)
         opt.zero_grad()
         loss.backward()
