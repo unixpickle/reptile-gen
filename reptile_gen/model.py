d7fe797da87a7b05aa8c5a319338c0c3380f7b00
Alex Nichol <unixpickle@gmail.com>
2019-10-20 13:33:17 -0400

    try a smaller, faster MNIST model

--- reptile_gen/model.py
+++ reptile_gen/model.py
@@ -6,6 +6,25 @@ import torch
 import torch.nn as nn
 
 
+class SmallMNISTModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.x_embed = nn.Embedding(28, 128)
+        self.y_embed = nn.Embedding(28, 128)
+        self.layers = nn.Sequential(
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 256),
+            nn.ReLU(),
+            nn.Linear(256, 1),
+        )
+
+    def forward(self, x):
+        x_vec = self.x_embed(x[:, 0])
+        y_vec = self.y_embed(x[:, 1])
+        return self.layers(torch.cat([x_vec, y_vec], dim=-1))
+
+
 class MNISTModel(nn.Module):
     def __init__(self):
         super().__init__()
