a6d8825309fcd57bc0e09ff1a9f7205b7ef885dc
Alex Nichol <unixpickle@gmail.com>
2019-10-20 18:21:01 -0400

    test batched model

---
+++ reptile_gen/model_test.py
@@ -0,0 +1,13 @@
+import numpy as np
+import torch
+
+from model import batch_mnist_model
+
+
+def test_batched_model():
+    m = batch_mnist_model()
+    m.to(torch.float64)
+    inputs = torch.from_numpy(np.array([[1, 2], [3, 4], [5, 1]])).long()[:, None]
+    batch_out = m.batch_forward(m.batch_parameters(3), inputs).detach().numpy()
+    regular_out = torch.stack([m(inputs[i]) for i in range(3)]).detach().numpy()
+    assert np.allclose(batch_out, regular_out)
